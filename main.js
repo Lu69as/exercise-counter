e806870e3f37e9d82ed11aacdcae6d6db413ba4c
lu69as <lukasokken@gmail.com>
2023-11-13 12:10:14 +0100

    A lot

--- main.js
+++ main.js
@@ -4,10 +4,6 @@ addExerciseBtn.addEventListener("click", () => addExercise(
     document.querySelector("#goal").value, 
     0, false)
 );
-const saveExerciseBtn = document.querySelector("button:nth-child(4)");
-saveExerciseBtn.addEventListener("click", () => addExerciseToLocalStorage(
-    null, null, null)
-);
 
 getExerciseFromLocalStorage();
 
@@ -31,7 +27,7 @@ function addExercise(exerciseValue, goalValue, amountValue, isfromls) {
     const amount = row.insertCell(2);
     const input = document.createElement("input");
     input.type = "number";
-    input.onkeyup = checkGoal
+    input.onchange = checkGoal;
     input.placeholder = 0;
     input.value = amountValue;
     amount.append(input);
@@ -47,15 +43,15 @@ function addExercise(exerciseValue, goalValue, amountValue, isfromls) {
     const button2 = document.createElement("button");
     button2.classList.add("button");
     button2.classList.add("add_button");
-    button2.onclick = addExerciseValue5;
-    button2.textContent = "Add 5";
+    button2.onclick = addExerciseValue;
+    button2.innerHTML = "Add <span>5</span>";
     Action.append(button2);
 
     const button3 = document.createElement("button");
     button3.classList.add("button");
     button3.classList.add("add_button");
-    button3.onclick = addExerciseValue10;
-    button3.textContent = "Add 10";
+    button3.onclick = addExerciseValue;
+    button3.innerHTML = "Add <span>10</span>";
     Action.append(button3);
 
     const button4 = document.createElement("button");
@@ -68,47 +64,33 @@ function addExercise(exerciseValue, goalValue, amountValue, isfromls) {
 }
 
 function complete() {
-    var index = this.parentNode.parentNode.rowIndex + 1;
-    if (document.querySelector('tr:nth-child('+index+') td:nth-child(3) input').value-0 >= 
-        document.querySelector('tr:nth-child('+index+') td:nth-child(2)').innerHTML-0) {
-        document.querySelector('tr:nth-child(2)').classList.add('completed-ani');
-        checkGoal()
+    let index = this.parentNode.parentNode;
+    let indexNum = index.rowIndex;
+    if (index.querySelector('input').value-0 >= index.querySelector('td:nth-child(2)').innerHTML-0) {
+        index.classList.add('completed-ani');
+        celebrate();
         setTimeout(() => {
-            addExerciseToLocalStorage(null, null, null)
-            document.querySelector(".exercise_table").deleteRow(index-1);
-            localStorage.removeItem('exercice' + (index-1).toString());
-            localStorage.removeItem('goal' + (index-1).toString());
-            localStorage.removeItem('amount' + (index-1).toString());
+            document.querySelector(".exercise_table").deleteRow(indexNum);
+            localStorage.removeItem('exercise' + (indexNum).toString());
+            addExerciseToLocalStorage(null, null, null);
         }, 2000);
     }
-    else {
-        alert('Your Amount of exercice does not match you set goal')
-    }
+    else alert('Your Amount of exercise does not match you set goal')
 } 
 
-function addExerciseValue5() {
-    var index = this.parentNode.parentNode.rowIndex + 1;
-    document.querySelector('tr:nth-child('+index+') td:nth-child(3) input').value = 
-    document.querySelector('tr:nth-child('+index+') td:nth-child(3) input').value - 0 + 5;
-    addExerciseToLocalStorage(null, null, null);
-    let goal = this.parentNode.parentNode.querySelector('td:nth-child(2)').innerHTML - 0;
-    if (this.parentNode.parentNode.querySelector('input').value - 0 >= goal) celebrate()
-} function addExerciseValue10() {
-    var index = this.parentNode.parentNode.rowIndex + 1;
-    document.querySelector('tr:nth-child('+index+') td:nth-child(3) input').value = 
-    document.querySelector('tr:nth-child('+index+') td:nth-child(3) input').value - 0 + 10;
+function addExerciseValue() {
+    input = this.parentNode.parentNode.querySelector('input');
+    input.value = input.value-0 + parseInt(this.querySelector('span').innerHTML);
     addExerciseToLocalStorage(null, null, null);
     let goal = this.parentNode.parentNode.querySelector('td:nth-child(2)').innerHTML - 0;
     if (this.parentNode.parentNode.querySelector('input').value - 0 >= goal) celebrate()
 }
 
 function removeExercise() {
-    addExerciseToLocalStorage(null, null, null)
     var index = this.parentNode.parentNode.rowIndex;
     document.querySelector(".exercise_table").deleteRow(index);
-    localStorage.removeItem('exercice' + index);
-    localStorage.removeItem('goal' + index);
-    localStorage.removeItem('amount' + index);
+    localStorage.removeItem('exercise' + index);
+    addExerciseToLocalStorage(null, null, null);
 }
 
 function clearInputValues() {
@@ -124,7 +106,7 @@ function clearInputValues() {
     for (let i = 1; i < rows.length; i++) {
         const td = rows[i].querySelector("td").textContent;
         if (td === exercise) {
-            console.error(`Error: "${exercise}" is already added!`);
+            alert.error(`Error: "${exercise}" is already added!`);
             return true;
         };
     }
@@ -133,65 +115,60 @@ function clearInputValues() {
 function addExerciseToLocalStorage(exercise, goal, amount) {
     const rows = document.querySelectorAll(".exercise_table tr");
     if (exercise === null) {
-        for (let i = 1; i <= 99 + 1; i++) {
-            localStorage.removeItem('exercice' + (i).toString());
-            localStorage.removeItem('goal' + (i).toString());
-            localStorage.removeItem('amount' + (i).toString());
-        }
-        for (let i = 2; i <= rows.length; i++) {
-            localStorage.setItem('exercice' + (i-1).toString(), document.querySelector('tr:nth-child('+i+') td:nth-child(1)').innerHTML);
-            localStorage.setItem('goal' + (i-1).toString(), document.querySelector('tr:nth-child('+i+') td:nth-child(2)').innerHTML);
-            localStorage.setItem('amount' + (i-1).toString(), document.querySelector('tr:nth-child('+i+') td:nth-child(3) input').value);
+        localStorage.clear();
+        for (let i = 1; i <= rows.length-1; i++) {
+            localStorage.setItem('exercise' + (i).toString(),
+                JSON.stringify([
+                    rows[i].childNodes[0].innerHTML,
+                    rows[i].childNodes[1].innerHTML-0,
+                    rows[i].querySelector('input').value-0,
+                ]) 
+            );
         }
     }
-    else {
-        localStorage.setItem('exercice' + rows.length, exercise);
-        localStorage.setItem('goal' + rows.length, goal);
-        localStorage.setItem('amount' + rows.length, amount);
-    }
+    else 
+        localStorage.setItem(
+            'exercise' + rows.length,
+            JSON.stringify([exercise, goal, amount])
+        );
 }
 
 function getExerciseFromLocalStorage() {
-    let localLength = localStorage.length;
-    for (let i = 1; i <= localLength; i++) {
-        if (localStorage.getItem('exercice' + i) === null) {
-            localStorage.removeItem('exercice' + i);
-            localStorage.removeItem('goal' + i);
-            localStorage.removeItem('amount' + i);
-        }
+    for (let i = 1; i <= localStorage.length; i++) {
+        if (localStorage.getItem('exercise' + i) === null)
+            localStorage.removeItem('exercise' + i);
         else {
-            addExercise(
-                localStorage.getItem('exercice' + i),
-                localStorage.getItem('goal' + i),
-                localStorage.getItem('amount' + i),
-                true
-            )
+            let exerciseLs = JSON.parse(localStorage.getItem('exercise'+i));
+            addExercise(exerciseLs[0], exerciseLs[1], exerciseLs[2], true)
         }
     }
 }
 
 function checkGoal() {
+    addExerciseToLocalStorage(null, null, null)
     let goal = this.parentNode.parentNode.querySelector('td:nth-child(2)').innerHTML - 0;
     if (this.value >= goal) celebrate();
 }
 
-// Check for keyboard input and add exercise if enter button is pressed
 document.querySelector('#goal').addEventListener("keydown", (e) => { 
     if (e.key !== "Enter") return;
     addExercise(document.querySelector("#exercise").value, document.querySelector("#goal").value, 0, false);
 })
 
-var myCanvas = document.createElement('canvas');
+let myCanvas = document.createElement('canvas');
 document.querySelector('#celebrate').appendChild(myCanvas);
 function celebrate() {
-    var myConfetti = confetti.create(myCanvas, {
-        resize: true,
-        useWorker: true
-    });
-    myConfetti({
-        particleCount: 500,
-        spread: 5000
-    });
+    for (let i = 0; i <= innerWidth / 100; i++) {
+        confetti({
+            particleCount: 50,
+            startVelocity: 30,
+            spread: 360,
+            origin: {
+                x: Math.random(),
+                y: Math.random() - 0.2
+            }
+        });
+    }
 }
 
 if (document.querySelectorAll(".exercise_table tr").length <= 1 ) {
